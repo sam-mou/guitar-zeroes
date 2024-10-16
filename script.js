09fee417f57033aad75e5dd944abe3da0b390636
“Samy <samy.mouahbi@hotmail.com>
2024-10-16 10:06:28 +0200

    Implement difficulty with more velocity depending on score and make the notes responsive

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 console.log('Guitar Zeroes is working!');
 
-const allKey = document.querySelectorAll(".keynote")
-const resetButton = document.getElementById("resetButton")
-const gameBoard = document.querySelector("#game-board")
-
-
+const allKey = document.querySelectorAll(".keynote");
+const resetButton = document.getElementById("resetButton");
+const gameBoard = document.querySelector("#game-board");
+const audio = document.getElementById("naive-music");
+const startButton = document.getElementById("startButton");
 const noteArray = [];
-const positionsArr = [70, 225, 381, 537]
 let score = 0;
-let lives = 3;
+let lives = 30;
+let currentVelocity = 1;
+let increasedVelocity = 5;
+
+// Calculate positions relative to the game board width
+function calculateGameBoardPositions() {
+    const gameBoard = document.getElementById('game-board');
+    const gameBoardWidth = gameBoard.offsetWidth;
+
+    const positionsArr = [
+        gameBoardWidth * 0.100,
+        gameBoardWidth * 0.325,
+        gameBoardWidth * 0.550,
+        gameBoardWidth * 0.775,
+    ];
+
+    return positionsArr;
+}
+
 
 // Class section
-
 class MusicNote {
     constructor() {
         this.width = 5;
         this.height = 5;
         this.color = 'black';
+        const positionsArr = calculateGameBoardPositions();
+
         this.positionX = positionsArr[Math.floor(Math.random() * positionsArr.length)];
         this.positionY = 100;
         this.domElement = null;
         this.createDomElement();
-        this.velocity = 1;
+        this.velocity = currentVelocity;
     }
 
     createDomElement() {
         this.domElement = document.createElement("div");
         this.domElement.className = "music-note";
         this.domElement.style.width = this.width + "vw";
-        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.height = this.height + "vw";
         this.domElement.style.left = this.positionX + "px";
         this.domElement.style.bottom = this.positionY + "vh";
         this.domElement.style.position = "absolute";
@@ -41,10 +59,8 @@ class MusicNote {
     }
 }
 
+
 // Interval section
-
-
-
 let noteCreationInterval = setInterval(() => {
     noteArray.push(new MusicNote())
 
@@ -56,7 +72,7 @@ let moveCreationInterval = setInterval(() => {
         //console.log(note);
 
 
-        if (note.positionY < -20) {
+        if (note.positionY < -5) {
             note.domElement.remove();
             noteArray.splice(i, 1);
             lives--;
@@ -74,6 +90,8 @@ function gameOver() {
         clearInterval(moveCreationInterval);
         clearInterval(noteCreationInterval);
         alert("Game over! Strings broken.. Your final score is: " + score);
+        audio.pause;
+        audio.currentTime = 0;
     }
 }
 
@@ -81,8 +99,11 @@ function gameOver() {
 function resetVariable() {
     noteArray.forEach(note => note.domElement.remove());
     noteArray.length = 0;
-    lives = 3;
+    lives = 30;
     score = 0;
+    currentVelocity = 1;
+    playSound;
+    audio.currentTime = 0;
     updateScoreAndLives();
     clearInterval(moveCreationInterval);
     clearInterval(noteCreationInterval);
@@ -94,7 +115,7 @@ function resetVariable() {
     moveCreationInterval = setInterval(() => {
         noteArray.forEach((note, i) => {
             note.moveDown();
-            if (note.positionY < -20) {
+            if (note.positionY < -5) {
                 note.domElement.remove();
                 noteArray.splice(i, 1);
                 lives--;
@@ -105,9 +126,20 @@ function resetVariable() {
     }, 15);
 }
 
+
+// Start button
+const playSound = startButton.addEventListener("click", function () {
+    audio.play();
+    audio.currentTime = 0;
+    audio.volume = 0.2;
+    console.log("Music started");
+});
+
+
 // Reset button
 resetButton.addEventListener("click", function () {
     resetVariable();
+    playSound;
 });
 
 // Hide reset button
@@ -116,59 +148,109 @@ function hideResetButton() {
 }
 
 // Show reset button
-
 function showResetButton() {
     resetButton.style.display = '';
 }
 
 
 // Score and lives display section
-
 function updateScoreAndLives() {
     document.getElementById("score").innerHTML = `Score: ${score}`;
     document.getElementById("lives").innerHTML = `Lives: ${lives}`;
 }
 
-function scoreCorrect(){
+// When score +1 we display a green screen
+function scoreCorrect() {
     gameBoard.style.backgroundColor = "green"
     setTimeout(() => {
-    gameBoard.style.backgroundColor = "white"
-
+        gameBoard.style.backgroundColor = "white"
     }, 300)
+}
+
+// When lives -1 we display a red screen
+function scoreWrong() {
+    gameBoard.style.backgroundColor = "red"
+    setTimeout(() => {
+        gameBoard.style.background = "white"
+    }, 300)
+}
+
+// When current score + 5 we increase our music notes velocity to increase the game difficulty
+function increaseGameDifficulty() {
+    if (score >= increasedVelocity) {
+        noteArray.forEach(function (note) {
+            note.velocity += 0.3;
+        });
+        currentVelocity += 0.3;
+        increasedVelocity += 5;
+    }
 }
 
 
 // Add Event Listenner section
-
 document.addEventListener('keydown', (e) => {
     let keyMatched = false;
+
+    // Get game board dimensions dynamically inside the event listener
+    const gameBoard = document.getElementById('game-board');
+    const gameBoardWidth = gameBoard.offsetWidth;
+    const gameBoardHeight = gameBoard.offsetHeight;
+
+    // Set my validation area
+    const yThreshold = gameBoardHeight * 0.01;
+    const yNegativeThreshold = -gameBoardHeight * 0.01;
+
     noteArray.forEach((musicNote, i) => {
-        if (e.code === "ArrowLeft" && musicNote.positionX === 70 && musicNote.positionY >= -10 && musicNote.positionY <= 10) {
-            console.log("left arrow clicked")
+        if (e.code === "ArrowLeft" &&
+            musicNote.positionX === gameBoardWidth * 0.100 &&
+            musicNote.positionY >= yNegativeThreshold &&
+            musicNote.positionY <= yThreshold) {
+
             score++;
             keyMatched = true;
-            scoreCorrect()
-        } else if (e.code === 'ArrowUp' && musicNote.positionX === 225 && musicNote.positionY >= -10 && musicNote.positionY <= 10) {
+            scoreCorrect();
+
+        } else if (e.code === 'ArrowUp' &&
+            musicNote.positionX === gameBoardWidth * 0.325 &&
+            musicNote.positionY >= yNegativeThreshold &&
+            musicNote.positionY <= yThreshold) {
+
             score++;
             keyMatched = true;
-            scoreCorrect()
-        } else if (e.code === 'ArrowDown' && musicNote.positionX === 381 && musicNote.positionY >= -10 && musicNote.positionY <= 10) {
+            scoreCorrect();
+
+        } else if (e.code === 'ArrowDown' &&
+            musicNote.positionX === gameBoardWidth * 0.550 &&
+            musicNote.positionY >= yNegativeThreshold &&
+            musicNote.positionY <= yThreshold) {
+
             score++;
             keyMatched = true;
-            scoreCorrect()
-        } else if (e.code === 'ArrowRight' && musicNote.positionX === 537 && musicNote.positionY >= -10 && musicNote.positionY <= 10) {
+            scoreCorrect();
+
+        } else if (e.code === 'ArrowRight' &&
+            musicNote.positionX === gameBoardWidth * 0.775 &&
+            musicNote.positionY >= yNegativeThreshold &&
+            musicNote.positionY <= yThreshold) {
+
             score++;
             keyMatched = true;
-            scoreCorrect()
+            scoreCorrect();
         }
-        musicNote.domElement.remove();
-        noteArray.splice(i, 1);
+
+        if (keyMatched) {
+            musicNote.domElement.remove();
+            noteArray.splice(i, 1);
+        }
     });
 
     if (keyMatched === false) {
         lives--;
+        scoreWrong();
     }
+
     console.log("Score: " + score + "Lives: " + lives);
+    increaseGameDifficulty();
     updateScoreAndLives();
     gameOver();
 });
